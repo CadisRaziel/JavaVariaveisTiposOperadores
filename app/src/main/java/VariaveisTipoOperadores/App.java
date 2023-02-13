79e7a55a03955832e5e9f4460bff7fb87952a8bb
vitor <vitorcsharp@hotmail.com>
2023-02-13 18:09:17 -0300

    VariaveisTipoOperadores

---
+++ app/src/main/java/VariaveisTipoOperadores/App.java
@@ -0,0 +1,20 @@
+
+package VariaveisTipoOperadores;
+
+public class App {
+    public static void main(String[] args) {
+       int idade = 0;
+       float dolar = 0.0f;
+       double preco = 0.0d;
+       char umaLetra = 'F'; // use aspas simples aqui ''
+       byte placar = 0;
+       boolean estaAtivo = false;
+       
+       
+       
+       String nome = "Use aspas duplas aqui";
+       
+       
+       
+    }
+}
